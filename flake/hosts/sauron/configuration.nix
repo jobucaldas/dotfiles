9549c808c266dd73f9a656478f58c976bc9d2619
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-07-20 19:55:56 -0300

    fix: make ssh work better

--- flake/hosts/sauron/configuration.nix
+++ flake/hosts/sauron/configuration.nix
@@ -277,6 +277,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    kitty.terminfo
+    git
     wget
     python3
     pfetch-rs
