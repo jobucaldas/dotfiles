846e5c14e0d9af88347a897ccd4ab12efa5ad7be
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-07-22 17:58:59 -0300

    feat: add findutils

--- flake/hosts/sauron/configuration.nix
+++ flake/hosts/sauron/configuration.nix
@@ -286,6 +286,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     kitty.terminfo
     git
+    findutils
     wget
     python3
     pfetch-rs
