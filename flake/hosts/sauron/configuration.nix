a94dbf4e379a43b0b51fdd8a2c1f7bfa8c137f81
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-07-22 17:56:07 -0300

    feat: add sunshine

--- flake/hosts/sauron/configuration.nix
+++ flake/hosts/sauron/configuration.nix
@@ -273,6 +273,13 @@
     useRoutingFeatures = "both";
   };
 
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
