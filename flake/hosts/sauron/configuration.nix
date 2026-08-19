f9e8f50b2e459b86de6eb054249af42b112d4c4b
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-08-19 15:33:50 -0300

    feat: simplify steam machine host config

--- flake/hosts/sauron/configuration.nix
+++ flake/hosts/sauron/configuration.nix
@@ -46,35 +46,6 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-
-      settings.General = {
-        Experimental = true;
-        Enable = "Source,Sink,Media,Socket";
-      };
-    };
-
-    xone.enable = true;
-  };
-
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 16 * 1024;
-    }
-  ];
-  zramSwap.enable = true;
-
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
@@ -88,13 +59,6 @@
     monaspace
   ];
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    desktopManager.xterm.enable = false;
-    excludePackages = [ pkgs.xterm ];
-  };
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "br";
@@ -103,29 +67,6 @@
 
   # Configure console keymap
   console.keyMap = "br-abnt2";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-
-    wireplumber = {
-      enable = true;
-    };
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -142,85 +83,11 @@
     packages = with pkgs; [ ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      nix-refresh = "nix flake update --flake \${HOME}/Projects/dotfiles/flake";
-      nix-test = "sudo nixos-rebuild test --flake \${HOME}/Projects/dotfiles/flake#$(hostname)";
-      nix-update = "sudo nixos-rebuild switch --flake \${HOME}/Projects/dotfiles/flake#$(hostname)";
-      nix-rollback = "sudo nixos-rebuild switch --rollback";
-      nix-clean = "nix store gc";
-    };
-
-    ohMyZsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "vi-mode"
-      ];
-      theme = "agnoster";
-    };
-
-    interactiveShellInit = ''
-      pfetch
-    '';
-  };
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-
-    gamescopeSession.enable = true;
-    protontricks.enable = true;
-  };
-
-  programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
-
-  jovian = {
-    steam = {
-      enable = true;
-      autoStart = true;
-      desktopSession = "gnome";
-      user = "jobu";
-    };
-
-    decky-loader = {
-      enable = true;
-      user = "jobu";
-      stateDir = "/home/jobu/.config/decky-loader";
-    };
-
-    hardware = {
-      has.amd.gpu = true;
-    };
-  };
-
-  services.tailscale = {
-    enable = true;
-    useRoutingFeatures = "both";
   };
 
   services.sunshine = {
