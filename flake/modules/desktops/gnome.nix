fe66e791a6fc03a8f55eb544143ddb996f6ce1ab
João Bueno <joaovictorbuenodecaldas@gmail.com>
2026-08-19 15:41:49 -0300

    feat: force sddm and add xdg portal for gnome

--- flake/modules/desktops/gnome.nix
+++ flake/modules/desktops/gnome.nix
@@ -10,5 +10,13 @@
   # Enable the GNOME Desktop Environment.
   # Jovian autoStart enables SDDM; GDM conflicts with it.
   services.displayManager.gdm.enable = false;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
 }
